cce9da87cb48a06b268a462c99892df599e50200
Jha2508 <57632370+Jha2508@users.noreply.github.com>
2019-12-24 12:11:28 +0530

    Create 1902003_IEEE_Task2.c
    Sensor tensor Task 2.0

---
+++ 1902003_IEEE_Task2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	int x;
+	printf("Enter the number to be converted to hexadecimal number:-");
+
+	scanf("%d",&x);
+	printf("Hexadecimal form is :-\n");
+	printf("%X",x);
+	printf("\n");
+	return 0;
+}

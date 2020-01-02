f79dd3cff99b9d7430f96cebabcbde9dbc7016b9
SauravYadav-dev <58463992+SauravYadav-dev@users.noreply.github.com>
2020-01-02 22:27:38 +0530

    Add files via upload

---
+++ 1904118_IEEE_task3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	long long num;
+	int decimalNum, i, rem;
+	
+	cout << "Enter any binary number:";
+	cin >> num;
+	
+	decimalNum = 0;
+	i = 0;
+	
+	while (num != 0)
+	{
+		rem = num % 10;
+		num /= 10;
+		decimalNum += rem * pow(2, i);
+		++i;
+	}
+	cout << "Equivalent Decimal number:" << decimalNum << endl;
+	return 0;
+
+}

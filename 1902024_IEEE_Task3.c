af0ea662cbd764681a5c21db3ba421ce1d0e1877
priyanshu-ui <58431516+priyanshu-ui@users.noreply.github.com>
2020-01-02 16:57:38 +0530

    1902024 Task 3 Electrical

---
+++ 1902024_IEEE_Task3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char bnum[1000];
+    scanf("%s", bnum);
+    int len = strlen(bnum);
+    int dnum = 0;
+    for(int i = 0; i<len; i++){
+        if(bnum[i]=='0'){dnum*=2;}
+        else if(bnum[i]=='1'){dnum=(dnum*2)+1;}
+    }
+    printf("%d", dnum);
+}
